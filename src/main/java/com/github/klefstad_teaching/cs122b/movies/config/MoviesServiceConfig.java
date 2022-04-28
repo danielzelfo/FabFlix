dfdea3a89dfd218db03e2db62586997afe1bb43f
Daniel Zelfo <dzelfo@uci.edu>
2022-04-27 17:49:59 -0700

    revert changes in source

--- src/main/java/com/github/klefstad_teaching/cs122b/movies/config/MoviesServiceConfig.java
+++ src/main/java/com/github/klefstad_teaching/cs122b/movies/config/MoviesServiceConfig.java
@@ -2,18 +2,9 @@ package com.github.klefstad_teaching.cs122b.movies.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @ConstructorBinding
 @ConfigurationProperties(prefix = "movie")
-public class MoviesServiceConfig extends WebSecurityConfigurerAdapter
+public class MoviesServiceConfig
 {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
-    }
 }
