0f3fcc384d92f140e4013610a362fb069752fd1a
Daniel Zelfo <dzelfo@uci.edu>
2022-05-07 01:44:23 -0700

    Added cart insert

--- src/main/java/com/github/klefstad_teaching/cs122b/billing/repo/BillingRepo.java
+++ src/main/java/com/github/klefstad_teaching/cs122b/billing/repo/BillingRepo.java
@@ -1,14 +1,41 @@
 package com.github.klefstad_teaching.cs122b.billing.repo;
 
+import com.github.klefstad_teaching.cs122b.billing.model.request.MovieRequest;
+import com.github.klefstad_teaching.cs122b.core.error.ResultError;
+import com.github.klefstad_teaching.cs122b.core.result.BillingResults;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import java.sql.Types;
 
 @Component
 public class BillingRepo
 {
+    private NamedParameterJdbcTemplate template;
+
     @Autowired
     public BillingRepo(NamedParameterJdbcTemplate template)
     {
+        this.template = template;
+    }
+
+    public void addToCart(Integer userId, MovieRequest movieRequest) {
+        String sql =
+                "INSERT INTO billing.cart (user_id, movie_id, quantity)" +
+                        "VALUES (:user_id, :movie_id, :quantity);";
+
+        MapSqlParameterSource source =
+                new MapSqlParameterSource()
+                        .addValue("user_id", userId, Types.INTEGER)
+                        .addValue("movie_id", movieRequest.getMovieId(), Types.INTEGER)
+                        .addValue("quantity", movieRequest.getQuantity(), Types.INTEGER);
+        try {
+            this.template.update(sql, source);
+        } catch (DuplicateKeyException exc) {
+            throw new ResultError(BillingResults.CART_ITEM_EXISTS);
+        }
     }
 }
--- src/main/java/com/github/klefstad_teaching/cs122b/billing/rest/CartController.java
+++ src/main/java/com/github/klefstad_teaching/cs122b/billing/rest/CartController.java
@@ -1,9 +1,23 @@
 package com.github.klefstad_teaching.cs122b.billing.rest;
 
+import com.github.klefstad_teaching.cs122b.billing.model.request.MovieRequest;
+import com.github.klefstad_teaching.cs122b.billing.model.response.BasicResponse;
 import com.github.klefstad_teaching.cs122b.billing.repo.BillingRepo;
 import com.github.klefstad_teaching.cs122b.billing.util.Validate;
+import com.github.klefstad_teaching.cs122b.core.error.ResultError;
+import com.github.klefstad_teaching.cs122b.core.result.BillingResults;
+import com.github.klefstad_teaching.cs122b.core.result.IDMResults;
+import com.github.klefstad_teaching.cs122b.core.security.JWTManager;
+import com.nimbusds.jwt.SignedJWT;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
 
 @RestController
 public class CartController
@@ -16,5 +30,27 @@ public class CartController
     {
         this.repo = repo;
         this.validate = validate;
+    }
+
+    @PostMapping("/cart/insert")
+    public ResponseEntity<BasicResponse> endpoint(@AuthenticationPrincipal SignedJWT user, @RequestBody MovieRequest movieRequest)
+    {
+        Integer userId;
+        try {
+            userId = user.getJWTClaimsSet().getIntegerClaim(JWTManager.CLAIM_ID);
+        } catch (ParseException exc) {
+            throw new ResultError(IDMResults.ACCESS_TOKEN_IS_INVALID);
+        }
+
+        if (movieRequest.getQuantity() == null || movieRequest.getQuantity() < 1)
+            throw new ResultError(BillingResults.INVALID_QUANTITY);
+        else if (movieRequest.getQuantity() > 10)
+            throw new ResultError(BillingResults.MAX_QUANTITY);
+
+        this.repo.addToCart(userId, movieRequest);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new BasicResponse()
+                        .setResult(BillingResults.CART_ITEM_INSERTED));
     }
 }
