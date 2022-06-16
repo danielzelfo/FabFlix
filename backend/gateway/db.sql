7055c57a947fb54a4e19b49f9f55116832ccd2cc
Daniel Zelfo <dzelfo@uci.edu>
2022-06-16 13:52:30 -0700

    clear

--- backend/gateway/db.sql
+++
@@ -1,9 +0,0 @@
-CREATE SCHEMA gateway;
-
-CREATE TABLE `gateway`.`request` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `ip_address` VARCHAR(64) NOT NULL,
-    `call_time` TIMESTAMP NOT NULL,
-    `path` VARCHAR(2048) NULL,
-    PRIMARY KEY (`id`)
-);
